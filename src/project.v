2c2823385c5faf005adf8127cc0bdc9ca2dca479
jxcelynzhu <114823228+jxcelynzhu@users.noreply.github.com>
2024-07-29 10:22:02 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,25 +15,6 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
   
-  /*
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
-    assign uo_out[0] = ui_in[0] + ui_in[1]; // OR gate assigned to output uo_out
-    assign uo_out[1] = ui_in[2] * ui_in[3]; // AND gate w/ inputs 2 and 3 assigned to ouput 1
-    assign uo_out[2] = ~ui_in[4]; // NOT gate
-    assign uo_out[3] = ~(ui_in[0] + ui_in[1]); // NOR gate
-    assign uo_out[4] = ~(ui_in[0] * ui_in[1]); // NAND gate
-
-    // Initializing wires
-    wire my_temp;
-    assign my_temp = ui_in[1];*/
-  
   //Output 0
   wire or0_ouA; // Output wire of first OR gate
   assign or0_ouA = ui_in[0] + ui_in[1]; // OR gate connecting inputs 0 and 1
